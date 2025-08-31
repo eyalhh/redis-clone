93e9591a7a47c2b99806b3f8ef9fa34cde3633dd
eyal <eyalz2007@gmail.com>
2025-08-31 22:42:46 +0300

    testing hashmap functionality

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "test.c"
 
 int main() {
 
     printf("hello world!\n");
+    test();
 
     return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
+#include "hashmap.h"
 
 void test() {
+
+    hashmap_t *hashmap = init_hashmap();
+    add_pair(hashmap, 212, "eyal");
+    add_pair(hashmap, 304, "roy");
+    add_pair(hashmap, 512, "maya");
+    print_hashmap(hashmap);
+    char *name = get_value(hashmap, 212);
+    printf("%s\n", name);
+    name = get_value(hashmap, 2000);
+    printf("%d\n", (int)name);
 
 }
