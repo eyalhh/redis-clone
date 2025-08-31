3e2f165075501167b5d90e9310a46b0b75eb7893
royshohat <roy123123sohat@gmail.com>
2025-08-31 23:42:23 +0300

    parsing and handaling input

--- main.c
+++ main.c
@@ -1,8 +1,55 @@
-#include <stdio.h>
+#include "hashmap.h"
+#include "main.h"
+
+int main_loop();
 
 int main() {
 
-    printf("hello world!\n");
-
+    main_loop();
     return 0;
 }
+
+int main_loop(){
+  enum func funcType; 
+  char cmd[MAX_CMD_SIZE];
+  char* token;
+  char** args = malloc(MAX_ARGS_COUNT * sizeof(char*));
+  int cmdCount;
+
+  while(TRUE){
+    cmdCount = 0;
+
+    printf(">> ");
+    fgets(cmd, sizeof(cmd), stdin);
+    
+    token = strtok(cmd, " ");
+
+    while(token != NULL){
+      if(cmdCount>=MAX_ARGS_COUNT){
+        printf("max args count execeded\n");
+        break;
+      }
+      args[cmdCount] = token;
+      token = strtok(NULL, " ");
+      cmdCount++;
+    }
+
+    if(strcmp("GET", args[0])) 
+      funcType = GET;
+    if(strcmp("SET", args[0])) 
+      funcType = SET;
+    if(strcmp("DEL", args[0])) 
+      funcType = DEL;
+    if(strcmp("EXIT", args[0])) 
+      funcType = EXIT;
+
+    // call each fucntion 
+
+  }
+  
+
+
+  // free args 
+  return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,11 @@
+#include <string.h>
+
+#define MAX_ARGS_COUNT 10
+#define MAX_CMD_SIZE 1024
+
+enum func{
+  GET, // 0
+  SET, // 1
+  DEL, // 2
+  EXIT // 3
+};
